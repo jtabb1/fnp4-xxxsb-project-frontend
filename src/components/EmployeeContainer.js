7121b31f13dcc23d05e24c21994627de4a92e182
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-09-15 07:01:45 -0400

    Draft empty EmployeeHire component

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { BASE_URL } from "../constraints/index.js";
 // import Ee from "./Employee.js";
+import EmployeeHire from "./EmployeeHire";
 import '../styles/EmployeeContainer.css'
 
 export default function EmployeeContainer() {
@@ -21,15 +22,15 @@ export default function EmployeeContainer() {
       <div>
           <h2 className="home-page-header">Employee List</h2>
           <ul>
-          {employees.map((employee) => (
-            <li key={employee.id}>
-              <span>
-                {employee.name},   {employee.public_id}
-              </span>
-              <Link to={`/employees/${employee.id}`}>Details</Link>
-            </li>
-          ))}
-      </ul>
+            {employees.map((employee) => (
+              <li key={employee.id}>
+                <span>
+                  {employee.name},   {employee.public_id}
+                </span>
+                <Link to={`/employees/${employee.id}`}>Details</Link>
+              </li>
+            ))}
+        </ul>
       </div>
   );
 }
